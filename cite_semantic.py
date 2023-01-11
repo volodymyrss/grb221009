417a5d6a6f50fead831a68285fa47e814dbde5b7
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2023-01-11 18:07:02 +0100

    new compton

--- cite_semantic.py
+++ cite_semantic.py
@@ -1,2 +1,9 @@
 def gen(modality, ref):
+    import requests    
+    # r = requests.get(ref, headers={'Accept': 'application/rdf+xml'})
+    r = requests.get(ref, headers={'Accept': 'application/x-bibtex; charset=utf-8'})    
+
+    with open("found-references.bib", "ab") as f:        
+        f.write(r.content)
+        
     return f"{modality}:{ref}"
